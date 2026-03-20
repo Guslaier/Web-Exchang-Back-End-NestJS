535173cb149d75246dbb7966bbfb35a64acdcc22
Guslaier <goolzxcv@gmail.com>
2026-03-21 02:51:50 +0700

    Refactor the use of Exceptions to reduce redundant code

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
+import { RedisModule } from '../redis/redis.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), RedisModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
