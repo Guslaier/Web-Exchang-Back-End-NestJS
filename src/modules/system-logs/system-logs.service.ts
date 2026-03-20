9cdaaaeb84e75a6097922518bf85e87e2993b834
XunziTH <pop3king@gmail.com>
2026-03-20 17:59:07 +0700

    add func for getLogs.

--- src/modules/system-logs/system-logs.service.ts
+++ src/modules/system-logs/system-logs.service.ts
@@ -1,8 +1,37 @@
-import { Injectable } from '@nestjs/common';
-import { SystemLog } from './entities/system-log.entity'; // Assuming you have a SystemLog entity defined
-import { CreateSystemLogDto } from './dto/system-log.dto'; // Assuming you have a DTO for creating system logs
-
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
+import { SystemLog  } from './entities/system-log.entity'; // Assuming you have a SystemLog entity defined
+import { CreateSystemLogDto , QueryDate } from './dto/system-log.dto'; // Assuming you have a DTO for creating system logs
+import { InjectRepository } from '@nestjs/typeorm';
+import { Between, Repository } from 'typeorm';
 @Injectable()
 export class SystemLogsService {
-   
+    constructor(
+        @InjectRepository(SystemLog)
+        private readonly systemLogRepo : Repository<SystemLog> , 
+    ){
+
+    }
+
+   async getAllByDate(currentUser : any , query : QueryDate ) {
+        const allowedRoles = ["ADMIN" , "MANAGER"] ; 
+        if(!allowedRoles.includes(currentUser.role)) {
+            throw new ForbiddenException("you are not allowed to request for logs.") ; 
+        }
+
+        const logs = await this.systemLogRepo.find({
+            where : {
+                createdAt : Between(query.startDate , query.endDate)  
+            } , 
+            order : {
+                createdAt : 'ASC'
+            }
+        }) ; 
+
+        if (!logs) {
+            throw new NotFoundException("logs are not found.") ; 
+        }
+
+        return logs ; 
+
+   }
 }
