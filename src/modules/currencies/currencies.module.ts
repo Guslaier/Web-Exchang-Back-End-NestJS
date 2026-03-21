62fc50470780f62c84cd9bbe11d6a6f8b5d03200
Guslaier <goolzxcv@gmail.com>
2026-03-21 23:16:56 +0700

    Currencise first commit

--- src/modules/currencies/currencies.module.ts
+++ src/modules/currencies/currencies.module.ts
@@ -1,11 +1,18 @@
+// currencies/currencies.module.ts
 import { Module } from '@nestjs/common';
-import { CurrenciesService } from './currencies.service';
-import { CurrenciesController } from './currencies.controller';
+import { HttpModule } from '@nestjs/axios';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CurrenciesService } from './currencies.service';
 import { Currency } from './entities/currency.entity';
+import { SystemLogsModule } from '../system-logs/system-logs.module';
+import { CurrenciesController } from './currencies.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Currency])],
+  imports: [
+    TypeOrmModule.forFeature([Currency]),
+    HttpModule, // เพื่อใช้ HttpService
+    SystemLogsModule,
+  ],
   controllers: [CurrenciesController],
   providers: [CurrenciesService],
   exports: [CurrenciesService],
