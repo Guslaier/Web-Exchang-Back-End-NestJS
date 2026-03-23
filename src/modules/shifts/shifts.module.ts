13fcad3b6e3ae7ab1dcde2ae0f88557dd13c3b41
XunziTH <pop3king@gmail.com>
2026-03-23 19:53:16 +0700

    set module.

--- src/modules/shifts/shifts.module.ts
+++ src/modules/shifts/shifts.module.ts
@@ -3,9 +3,11 @@ import { ShiftsService } from './shifts.service';
 import { ShiftsController } from './shifts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Shift } from './entities/shift.entity';
+import { BoothsModule } from '../../modules/booths/booths.module';
+import { SystemLogsModule } from '../../modules/system-logs/system-logs.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Shift])],
+  imports: [TypeOrmModule.forFeature([Shift]) , BoothsModule , SystemLogsModule],
   controllers: [ShiftsController],
   providers: [ShiftsService],
   exports: [ShiftsService],
