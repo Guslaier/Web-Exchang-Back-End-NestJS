5639231e211464f3c532fd1b7a81ccf0025e8162
XunziTH <pop3king@gmail.com>
2026-03-31 18:41:58 +0700

    feature:crate for insert to supertype trans

--- src/modules/transactions/transactions.service.ts
+++ src/modules/transactions/transactions.service.ts
@@ -1,7 +1,44 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable , InternalServerErrorException } from '@nestjs/common';
+import { EntityManager} from 'typeorm';
+import { Transaction } from './entities/transaction.entity';
 
 @Injectable()
 export class TransactionsService {
-    
+    async create(manager : EntityManager , type : string) {
+
+        if (!(type === 'TRANSFER')  && !(type === 'EXCHANGE')) {
+            throw new InternalServerErrorException('Invalid transaction type from service');
+        }
+
+        const transactionRepository = manager.getRepository(Transaction); 
+
+        const transaction = transactionRepository.create({
+            type : type,
+        });
+
+        for (let i = 0; i < 3; i++) {
+            transaction.id = this.generateId();
+            try {
+                await transactionRepository.save(transaction);
+                return transaction;
+            } catch (error) {
+                console.error(`Attempt ${i + 1} to save transaction failed:`, error);
+            }
+        }
+        throw new InternalServerErrorException('Failed to create transaction');
+    }
+
+    generateId() {
+        const now = new Date();
+        const timestamp = now.getFullYear().toString() + '-' +
+        (now.getMonth() + 1).toString().padStart(2, '0') + '-' +
+        now.getDate().toString().padStart(2, '0') + 'T' +
+        now.getHours().toString().padStart(2, '0') + ':' + 
+        now.getMinutes().toString().padStart(2, '0') + ':' +
+        now.getSeconds().toString().padStart(2, '0') + '-'; 
+        
+        const randomHex  = Math.floor(Math.random() * 0xFFFF).toString(16).padStart(4, '0').toUpperCase();
+        
+        return `${timestamp}${randomHex}`;
+    }
 }
