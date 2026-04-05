fc27860461862513fbbaadbee33c81784feaa8ee
XunziTH <pop3king@gmail.com>
2026-04-05 22:18:52 +0700

    feature : log for log for using cashcount service.

--- src/modules/cash-counts/cash-counts.service.ts
+++ src/modules/cash-counts/cash-counts.service.ts
@@ -1,7 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { SystemLogsService } from '../../modules/system-logs/system-logs.service';
+import { EntityManager } from 'typeorm';
 // Assuming you have an entity for cash count
 
 @Injectable()
 export class CashCountsService {
+
+    constructor(
+        protected readonly systemLogsService: SystemLogsService,
+    ) {
+
+    }
+    
+    private async log(user: any, action: string, details: string, manager?: EntityManager) {
+            await this.systemLogsService.createLog(
+              user,
+              {
+                userId: user?.id || null,
+                action,
+                details,
+              },
+              manager, // ส่งต่อ manager เพื่อให้อยู่ใน Transaction เดียวกัน
+            );
+    }
     
 }
