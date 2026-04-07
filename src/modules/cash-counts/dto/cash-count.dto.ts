2635e90a5b5c3389b34463011341b85c25cb2288
XunziTH <pop3king@gmail.com>
2026-04-07 21:32:44 +0700

    feat : createCashCountDto , DenominationDto , AmountDto

--- src/modules/cash-counts/dto/cash-count.dto.ts
+++ src/modules/cash-counts/dto/cash-count.dto.ts
@@ -1,15 +1,36 @@
-import { IsString, IsNotEmpty, IsNumber } from 'class-validator';
+import { IsString, IsNotEmpty, IsNumber, IsUUID, IsArray, ArrayNotEmpty, Validate, ValidateNested, IsOptional } from 'class-validator';
+import { Type } from 'class-transformer';
 
-export class CreateCashCountDto {
+class DenominationDto {
+   @IsString()
+   @IsNotEmpty()
+   denomination: string; 
+}
+
+class AmountDto {
+    @IsNumber()
+    @IsNotEmpty()
+    amount: number; 
+}
+
+export class CreateCashCountDto { 
   @IsString()
   @IsNotEmpty()
-  shiftId: string;
+  transactionId : string ;
 
-  @IsNumber()
-  @IsNotEmpty()
-  amount: number;
+  @IsUUID()
+  @IsOptional()
+  currencyId ?: string ;
 
-  @IsString()
-  @IsNotEmpty()
-  currencyCode: string;
+  @IsArray()
+  @ArrayNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => DenominationDto)
+  denominations : DenominationDto[] ; 
+
+  @IsArray()
+  @ArrayNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => AmountDto)
+  amounts : AmountDto[] ;
 }
