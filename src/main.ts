98cbaf7d1bfad2edd9cd713ab04db5f53943d68b
XunziTH <pop3king@gmail.com>
2026-04-07 21:21:19 +0700

    chore: whitefilering with dto.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 3000;
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+    transformOptions : {
+      enableImplicitConversion : true
+    }
+  }));
+
   await app.listen(PORT);
   console.log(`Application is running on: http://localhost:${PORT}`);
 }
