30fd51b8117a729ca4540ad7be30e2f19d904aee
XunziTH <pop3king@gmail.com>
2026-04-12 16:54:40 +0700

    feat(Customer) : add Get to req for cumtomer passport img.

--- src/modules/customers/customers.controller.ts
+++ src/modules/customers/customers.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Post, Get, Param, Body } from '@nestjs/common';
+import { Controller, Post, Get, Param, Body , UseGuards, Query , Header, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { CustomersService } from './customers.service';
-import { CreateCustomerDto } from './dto/customer.dto';
+import { CreateCustomerDto, GetImgDto } from './dto/customer.dto';
 import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
 import { RolesGuard } from '../../common/guards/roles.guard';
 import { Roles } from '../../common/decorators/roles.decorator';
+import { CurrentUser } from '../../common/decorators/current-user.decorator';
+import * as mime from 'mime-types';
+
 
 @Controller('customers')
 export class CustomersController {
@@ -14,8 +18,14 @@ export class CustomersController {
     return ;
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return ;
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('EMPLOYEE' , 'MANAGER' , 'ADMIN')
+  @Get()
+  findOne(@CurrentUser() currentUser : any , @Query() getImgDto : GetImgDto , @Res({passthrough : true}) res : Response) {
+
+    const contentType = mime.lookup(getImgDto.passportImg) || 'application/octet-stream';
+    res.setHeader('Content-Type', contentType); 
+    
+    return this.customersService.getImg(currentUser , getImgDto)  ;
   }
 }
