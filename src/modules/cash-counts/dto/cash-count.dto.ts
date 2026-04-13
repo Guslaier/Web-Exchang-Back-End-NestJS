1f1276406c5efe2e52e3881017ea59303a4ac5fa
XunziTH <pop3king@gmail.com>
2026-04-13 17:33:25 +0700

    refactor(CashCount) : DTO implement CashCountData to all dto.

--- src/modules/cash-counts/dto/cash-count.dto.ts
+++ src/modules/cash-counts/dto/cash-count.dto.ts
@@ -1,19 +1,20 @@
 import { IsString, IsNotEmpty, IsNumber, IsUUID, IsArray, ArrayNotEmpty, Validate, ValidateNested, IsOptional } from 'class-validator';
 import { Type } from 'class-transformer';
+import {CashCountData} from './../../../types/index'; ;
 
-class DenominationDto {
+class DenominationDto implements Pick<CashCountData, 'denomination'> {
    @IsString()
    @IsNotEmpty()
    denomination: string; 
 }
 
-class AmountDto {
+class AmountDto implements Pick<CashCountData, 'amount'>  {
     @IsNumber()
     @IsNotEmpty()
     amount: number; 
 }
 
-export class CreateCashCountDto { 
+export class CreateCashCountDto implements Pick<CashCountData,   'transactionId'> { 
   @IsString()
   @IsNotEmpty()
   transactionId : string ;
