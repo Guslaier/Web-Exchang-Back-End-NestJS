3ab0c1ce31f7736db49e267f7fbbb0c6536ad630
XunziTH <pop3king@gmail.com>
2026-04-19 21:20:05 +0700

    feat(sse) : controller Get refresh-signal.

---
+++ src/modules/sse/sse.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Res , Sse , UseGuards , MessageEvent} from "@nestjs/common";
+import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
+import { RolesGuard } from '../../common/guards/roles.guard';
+import { Roles } from '../../common/decorators/roles.decorator';
+import { Observable , map} from "rxjs";
+import { SseService } from "./sse.service";
+
+@Controller('sse')
+export class SseController {
+    constructor(private readonly sseService: SseService) {}
+
+    @Sse('refresh-signal')
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles("ADMIN" , "MANAGER" , "EMPLOYEE")
+    connectToSse()  : Observable<MessageEvent> {
+        return this.sseService.getRefreshSignal().pipe(
+            map((signal) => ({ data:  JSON.stringify(signal) }))
+        );
+    }
+}
