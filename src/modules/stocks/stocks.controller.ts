763a161da3d8cd20712b7de76cb9e50198fd5582
XunziTH <pop3king@gmail.com>
2026-05-01 22:47:03 +0700

    feat(stocks) : controller feat Get stock/shift for get thai stock info of the shift/

--- src/modules/stocks/stocks.controller.ts
+++ src/modules/stocks/stocks.controller.ts
@@ -1,6 +1,23 @@
-import { Controller } from "@nestjs/common";
+
+import { Controller , Get, UseGuards  , Query, Inject} from "@nestjs/common";
+import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
+import { RolesGuard } from '../../common/guards/roles.guard';
+import { Roles } from '../../common/decorators/roles.decorator';
+import { CurrentUser } from '../../common/decorators/current-user.decorator';
+import { GetStockShiftQuery } from './dto/stocks.dto' ;
+import { StocksService } from './stocks.service' ;
 
 @Controller('stocks')
 export class StocksController {
-    constructor() {}
+    constructor(
+        @Inject(StocksService)
+        private readonly stockService : StocksService , 
+    ) {}
+    
+    @UseGuards(JwtAuthGuard,RolesGuard)
+    @Roles('ADMIN','MANAGER','EMPLOYEE')
+    @Get('shift')
+    getStockShift(@CurrentUser() user : any , @Query() query : GetStockShiftQuery  ) {
+        return this.stockService.getTHBSummary(user , query.id ) ; 
+    }
 }
