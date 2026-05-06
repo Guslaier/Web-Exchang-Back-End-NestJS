fd754a1f7c15e5843b087360da48f1bca391907d
XunziTH <pop3king@gmail.com>
2026-05-06 19:31:56 +0700

    feat(report) : Dto getPreviousShift

--- src/modules/reports/dto/report.dto.ts
+++ src/modules/reports/dto/report.dto.ts
@@ -1,8 +1,14 @@
 import { IsNumber , IsNotEmpty , Min , IsObject , ValidateNested, IsUUID} from 'class-validator' ; 
 import { Type } from 'class-transformer';
-import type  { ShiftData } from './../../../types/index' ;  
+import type  { ShiftData , BoothData} from './../../../types/index' ;  
 import { CreateCashCountDto } from './../../cash-counts/dto/cash-count.dto' ; 
 
+
+export class GetPreviousShift implements Pick<BoothData , 'id'> {
+    @IsUUID()
+    @IsNotEmpty()
+    id : string ; 
+}
 
 export class PutShiftParam implements Pick<ShiftData , 'id'> {
     @IsUUID() 
