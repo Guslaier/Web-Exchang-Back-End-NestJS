4643a04fd7b6fd72ce8735a7d15948ac9748fc7d
XunziTH <pop3king@gmail.com>
2026-05-06 19:29:49 +0700

    feat(report) : module import services.

--- src/modules/reports/reports.module.ts
+++ src/modules/reports/reports.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { ReportsController} from './reports.controller' ; 
 import { ReportsService } from './reports.service' ; 
 import { ShiftsModule } from './../shifts/shifts.module' ;
-// import { Tra} 
+import { StocksModule } from "./../stocks/stocks.module";
+import { CashCountsModule } from "./../cash-counts/cash-counts.module" ; 
 
 @Module({
-    imports :[ShiftsModule] ,
+    imports :[ShiftsModule , StocksModule , CashCountsModule] ,
     controllers : [ReportsController] , 
     providers : [ReportsService] , 
     exports :[ReportsService] ,
