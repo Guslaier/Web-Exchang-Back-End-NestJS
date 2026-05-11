1b795ee7dbf4d97d15450a4facfa373b20de2ad3
XunziTH <pop3king@gmail.com>
2026-05-11 18:07:20 +0700

    feat(transaction) : service getPendingTransId(get pending transactions with shift id).

--- src/modules/transactions/transactions.service.ts
+++ src/modules/transactions/transactions.service.ts
@@ -1,10 +1,19 @@
 import { Injectable , InternalServerErrorException } from '@nestjs/common';
-import { EntityManager} from 'typeorm';
+import { EntityManager, Repository} from 'typeorm';
 import { Transaction } from './entities/transaction.entity';
 import { CreateTransactionDto } from './dto/transaction.dto';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class TransactionsService {
+
+    constructor(
+        @InjectRepository(Transaction)
+        private readonly transactionRepo : Repository<Transaction>
+    ){
+
+    }
+
     async create(manager : EntityManager , createTransactionDto: CreateTransactionDto)  {
 
         const transactionRepository = manager.getRepository(Transaction); 
@@ -38,5 +47,14 @@ export class TransactionsService {
         const randomHex  = Math.floor(Math.random() * 0xFFFF).toString(16).padStart(4, '0').toUpperCase();
         
         return `${timestamp}${randomHex}`;
+    }
+
+    async getPendingTransId(id : string) {
+        const pendingTransactionsId : any[] =  await this.transactionRepo.query(
+            `select t.id from transactions t 
+            join exchange_transactions et on t.id = et.id
+            where et.status  = $1 and t."shiftId" = $2 ; `,['PENDING' , id]) ;
+
+        return pendingTransactionsId ; 
     }
 }
